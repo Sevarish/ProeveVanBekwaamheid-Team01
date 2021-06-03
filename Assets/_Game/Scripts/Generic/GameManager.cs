f1f158bc607230f921147e89a5763f0646846f8f
Jochem Lammertsma <30724588+jochemlam@users.noreply.github.com>
2021-06-03 14:13:04 +0200

    created working gameplay loop

--- Assets/_Game/Scripts/Generic/GameManager.cs
+++ Assets/_Game/Scripts/Generic/GameManager.cs
@@ -6,18 +6,28 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private Player player;
+    private ObjectiveManager gameState;
 
     private void Awake()
     {
         player = GameObject.FindObjectOfType<Player>();
+        gameState = GameObject.FindObjectOfType<ObjectiveManager>();
         player.GameOver += ResetScene;
+        gameState.GameWon += FinishScene;
     }
 
     public void ResetScene() => StartCoroutine(ResetSceneTimer());
+    public void FinishScene() => StartCoroutine(CompleteSceneTimer());
 
     private IEnumerator ResetSceneTimer()
     {
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("GameOver");
+    }
+
+    private IEnumerator CompleteSceneTimer()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("WinScreen");
     }
 }
